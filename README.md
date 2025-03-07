b405bf1b3daa73d27d579d80e96b91502b21ff41
Mike Cunneen <mike@appworkshop.net>
2025-03-07 15:28:36 +0800

    refactor: :arrow_up: yarn => npm. Provide better output.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 A command line tool to help you initialize a new git-based project or fork with:
 
+- GitHub remote
 - enforcement of conventional commit comments (commitlint and husky)
 - changelog generation (git-cliff)
 - dotenvx secret management via .env files
@@ -13,4 +14,15 @@ A command line tool to help you initialize a new git-based project or fork with:
 This repository itself was created with `gravity-assist`.
 
 Run `npx gravity-assist` for an interactive session.
+
+## Requirements
+
+The following tools must be installed on your machine:
+
+- [node](https://nodejs.org/) - at least v18
+- [git](https://git-scm.com/)
+- [nvm](https://github.com/nvm-sh/nvm)
+- [npm](https://www.npmjs.com/)
+- [gh](https://cli.github.com/)
+- [jq](https://stedolan.github.io/jq/)
 
--- gravity-assist.sh
+++ gravity-assist.sh
@@ -10,37 +10,20 @@
 
 # ==== CONSTANTS ====
 # prerequisites must be in the path
-PREREQUISITES="git nvm yarn gh curl realpath dirname"
+PREREQUISITES="git nvm npm jq gh curl realpath dirname node"
 # Date as YYYYMMDD
 DATE=$(date +%Y%m%d)
-# node minimum version for .nvmrc
-NODE_MIN_VERSION=20
+# node minimum version to run this script
+NODE_MIN_VERSION=18
 
 # ==== CONFIGURATION DEFAULT VALUES FOR PROMPTS - change as needed ====
 DEFAULT_NEW_OR_EXISTING=existing
 DEFAULT_NEW_FOLDER_NAME=gravity-assist
+DEFAULT_NEW_REPO_PUBLIC_OR_PRIVATE=public
 DEFAULT_EXISTING_REPO_TO_FORK=https://github.com/cunneen/gravity-assist.git
+# node minimum version for .nvmrc
+DEFAULT_NVM_NODE_MIN_VERSION=20
 
-# ---- Configuration - prompt user for values, falling back to defaults ---
-if [ ${INITIALISED:-0} -ne 1 ]; then
-  # it's the first time
-  echo "=== CONFIGURATION ==="
-
-  read -p "Fork an existing repo, or create a completely new one? (new|existing)[$DEFAULT_NEW_OR_EXISTING]: " NEW_OR_EXISTING
-  if [ "${NEW_OR_EXISTING}" == "existing" ]; then
-    read -p "Provide the full URL to the repo you want to fork (e.g. "https://github.com/cunneen/gravity-assist.git")[$DEFAULT_EXISTING_REPO_TO_FORK]: " EXISTING_REPO_TO_FORK
-  fi
-  read -p "What is the name of the folder you want to create? [$DEFAULT_NEW_FOLDER_NAME]: " NEW_FOLDER_NAME
-
-  # fall back to defaults where no values are provided
-  NEW_OR_EXISTING=${NEW_OR_EXISTING:-$DEFAULT_NEW_OR_EXISTING}
-  EXISTING_REPO_TO_FORK=${EXISTING_REPO_TO_FORK:-$DEFAULT_EXISTING_REPO_TO_FORK}
-  NEW_FOLDER_NAME=${NEW_FOLDER_NAME:-$DEFAULT_NEW_FOLDER_NAME}
-fi
-
-INITIALISED=1 # set to 1 so that the script will not prompt again
-
-# ==== END OF CONFIGURATION ====
 
 # ===== Error handling =======
 # set -e: exit on error
@@ -53,54 +36,149 @@ trap 'catch $? $LINENO' EXIT
 catch() {
   if [ "$1" != "0" ]; then
     # error handling goes here
-    echo "Error $1 occurred on $2" >&2
+    echo "(Exiting due to return code $1 on line $2)" >&2
   else
     echo "Script completed successfully"
   fi
 }
 
-######### test for prerequisites
 
-# load nvm
-
+# ==== TEST FOR PREREQUISITES ====
 for cmd in ${PREREQUISITES}
 do
   echo "checking for ${cmd}"
   if [ "${cmd}" == "nvm" ]; then
+    # load nvm
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
   else
     command -v ${cmd} >/dev/null 2>&1 || {
-      echo "!!!!!!!! ERROR !!!!!!!!\n\n" >&2
-      echo "I require ${cmd} but it's not in your PATH. \n\n" >&2
-      echo "PATH:${PATH}. \n\n" >&2
+      echo -e "===== ERROR - MISSING PREREQUISITE =====\n" >&2
+      echo -e "PATH:\n$(echo $PATH | tr ':' '\n' | sed 's/^/  /')\n" >&2
+      echo -e "-----------------------------------------\n" >&2
+      echo -e "ERROR: I require ${cmd} but it's not in your PATH. \n\n" >&2
       exit 1
     }
   fi
 done
 
-# get the current dir
+# check node version
+NODE_VERSION=$(node -v  | cut -d '.' -f 1,2 | cut -d 'v' -f 2)
+echo "comparing NODE_VERSION=${NODE_VERSION} with NODE_MIN_VERSION=${NODE_MIN_VERSION}"
+if [ ${NODE_VERSION} \< ${NODE_MIN_VERSION} ]; then
+  echo -e "\n\n===== ERROR - NODE VERSION =====\n\n" >&2
+  echo "ERROR: This script requires node >= ${NODE_MIN_VERSION}; you are running node ${NODE_VERSION}"
+  exit 1
+fi
+
+# ---- Configuration - prompt user for values, falling back to defaults ---
+echo "=== CONFIGURATION ==="
+echo "Please answer a few questions to configure this script, or Ctrl-C to exit."
+echo "--"
+while [ "${NEW_OR_EXISTING}" != "new" ] && [ "${NEW_OR_EXISTING}" != "existing" ]; do
+  read -p "Fork an existing repo, or create a completely new one? (new|existing)[default:$DEFAULT_NEW_OR_EXISTING]: " NEW_OR_EXISTING
+  # fall back to defaults where no values are provided
+  NEW_OR_EXISTING=${NEW_OR_EXISTING:-$DEFAULT_NEW_OR_EXISTING}
+  if [ "${NEW_OR_EXISTING}" != "new" ] && [ "${NEW_OR_EXISTING}" != "existing" ]; then
+    echo "Please enter 'new' to create a new repo, or 'existing' to fork an existing one."
+  fi
+done
+echo "${NEW_OR_EXISTING} repository selected."
+
+if [ "${NEW_OR_EXISTING}" == "existing" ]; then
+  echo "--"
+  echo "This script currently only works with GitHub repositories."
+  while [ "${EXISTING_REPO_TO_FORK}" == "" ] || [[ ! "${EXISTING_REPO_TO_FORK}" =~ ^https://github.com/ ]] ; do
+    read -p "Provide the full URL to the repo you want to fork (e.g. "https://github.com/cunneen/gravity-assist.git")[default:$DEFAULT_EXISTING_REPO_TO_FORK]: " EXISTING_REPO_TO_FORK
+    # fall back to defaults where no values are provided
+    EXISTING_REPO_TO_FORK=${EXISTING_REPO_TO_FORK:-$DEFAULT_EXISTING_REPO_TO_FORK}
+    if [[ ! "${EXISTING_REPO_TO_FORK}" =~ ^https://github.com/ ]]; then
+      echo "Please provide a GitHub URL."
+    fi
+  done
+  echo "${EXISTING_REPO_TO_FORK} selected."
+elif [ "${NEW_OR_EXISTING}" == "new" ]; then
+  echo "--"
+  while [ "${NEW_REPO_PUBLIC_OR_PRIVATE}" != "public" ] && [ "${NEW_REPO_PUBLIC_OR_PRIVATE}" != "private" ]; do
+    read -p "Should the new GitHub repo be public or private? (public|private)[default:$DEFAULT_NEW_REPO_PUBLIC_OR_PRIVATE]: " NEW_REPO_PUBLIC_OR_PRIVATE
+    # fall back to defaults where no values are provided
+    NEW_REPO_PUBLIC_OR_PRIVATE=${NEW_REPO_PUBLIC_OR_PRIVATE:-$DEFAULT_NEW_REPO_PUBLIC_OR_PRIVATE}
+    if [ "${NEW_REPO_PUBLIC_OR_PRIVATE}" != "public" ] && [ "${NEW_REPO_PUBLIC_OR_PRIVATE}" != "private" ]; then
+      echo "Please enter 'public' or 'private'."
+    fi
+  done
+  echo "${NEW_REPO_PUBLIC_OR_PRIVATE} selected."
+
+  echo "--"
+  echo "--"
+  echo "A .nvmrc file will be created in the new folder, specifying the minimum node version."
+  echo "If you're not using node js, just accept the default value."
+  read -p "Minimum node version [$DEFAULT_NVM_NODE_MIN_VERSION]: " NVM_NODE_MIN_VERSION
+  NVM_NODE_MIN_VERSION=${NVM_NODE_MIN_VERSION:-$DEFAULT_NVM_NODE_MIN_VERSION}
+  echo "Minimum node version will be set to ${NVM_NODE_MIN_VERSION}."
+
+fi
+
+echo "--"
+echo "A new folder will be created in your current working directory $(PWD)."
+read -p "What is the name for the NEW folder (and project)? [$DEFAULT_NEW_FOLDER_NAME]: " NEW_FOLDER_NAME
+NEW_FOLDER_NAME=${NEW_FOLDER_NAME:-$DEFAULT_NEW_FOLDER_NAME}
+echo "${NEW_FOLDER_NAME} selected as new project/folder name."
+
+
+# ==== END OF CONFIGURATION ====
+
+# get the current dir for this script
 reldir=$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")
 DIR="$(realpath "${PWD}/${reldir}")"
 
 # ===== MAIN SCRIPT =======
 
+echo "---- Checking gh auth status ... ----"
+(gh auth status || gh auth login)
+
 if [ "${NEW_OR_EXISTING}" == "new" ]; then
-  mkdir -p "${NEW_FOLDER_NAME}" && cd "${NEW_FOLDER_NAME}"
-
-  echo ${NODE_MIN_VERSION} > .nvmrc && nvm use ${NODE_MIN_VERSION}
-
+  echo "---- Setting up ${NEW_FOLDER_NAME} ... ----"
+  mkdir -p "${NEW_FOLDER_NAME}" && command cd "${NEW_FOLDER_NAME}"
+  echo "---- Setting node version to ${NVM_NODE_MIN_VERSION} ... ----"
+  echo ${NVM_NODE_MIN_VERSION} > .nvmrc && nvm use ${NVM_NODE_MIN_VERSION}
+  echo "---- Initialising git ... ----"
   git init .
-  yarn init
+  echo "# foobar" >> README.md
+  git init
+  git add README.md
+  git branch -M main
+  git commit -m "chore: Add README.md"
+  echo "---- choose a license ... ----"
+  npx license
+  echo "---- Initialising npm ... ----"
+  npm init
+  npm pkg set "private"=true
+  echo "---- Initialising GitHub repo ----"
 
-  gh repo create ${NEW_FOLDER_NAME} --public
+  gh repo create ${NEW_FOLDER_NAME} \
+    --${NEW_REPO_PUBLIC_OR_PRIVATE} \
+    --add-readme \
+    --remote origin \
+    --source .
 
 # elseif forking a github repo
 elif [ "${NEW_OR_EXISTING}" == "existing" ]; then
-
+  echo "---- Forking ${EXISTING_REPO_TO_FORK} and cloning to ${NEW_FOLDER_NAME} ... ----"
   gh repo fork ${EXISTING_REPO_TO_FORK} --remote --clone ${NEW_FOLDER_NAME}
   command cd ${NEW_FOLDER_NAME}
+
+  if [ -f "package.json" ]; then
+    echo "---- Initialising npm ... ----"
+    echo "Found existing package.json file. Backing up to package.json.bak"
+    cp package.json package.json.bak
+    echo "running npm init"
+  else
+    echo "---- Initialising npm ... ----"
+    echo "No package.json file found. Creating one."
+  fi
+  npm init
 
 fi
 
@@ -115,42 +193,51 @@ curl -s https://www.toptal.com/developers/gitignore/api/visualstudiocode,node,os
 
 echo "---- setting up @dotenvx/dotenvx ----";
 
-yarn add -D @dotenvx/dotenvx
+npm add --save-dev @dotenvx/dotenvx
 
 ./node_modules/.bin/dotenvx ext gitignore --pattern .env.keys
 ./node_modules/.bin/dotenvx  ext gitignore --pattern '!/.env'
 
 echo "HELLO=World" >> .env
-echo "console.log('Hello ' + process.env.HELLO)" > sayhello.js
+mkdir -p test
+echo "console.log('Hello ' + process.env.HELLO)" > test/testdotenvx.js
 
 # encrypt your .env file; this also creates a .env.keys file.
 ./node_modules/.bin/dotenvx encrypt
 
 # update package.json to have a 'testdotenvx' script
-npm pkg set scripts.testdotenvx="dotenvx run -- node sayhello.js"
+npm pkg set scripts.testdotenvx="dotenvx run -- node test/testdotenvx.js"
 
-yarn run testdotenvx
+npm run testdotenvx
 
 #### commitlint
 echo "---- setting up commitlint ----";
 
-yarn add --dev @commitlint/{cli,config-conventional}
+npm add --save-dev @commitlint/{cli,config-conventional}
 echo "module.exports = { extends: ['@commitlint/config-conventional'] };" > .commitlintrc.js
 
 #### husky
 echo "---- setting up husky ----";
 
-yarn add --dev husky
-yarn husky init
+npm add --save-dev husky
+./node_modules/.bin/husky init
+
+if [ ! -f "${HOME}/.config/husky/init.sh" ]; then
+  echo "creating ~/.config/husky/init.sh to load nvm"
+  mkdir -p "${HOME}/.config/husky"
+  touch "${HOME}/.config/husky/init.sh"
+  echo "export NVM_DIR=\"\$([ -z \"\${XDG_CONFIG_HOME-}\" ] && printf %s \"\$HOME/.nvm\" || printf %s \"\$XDG_CONFIG_HOME/nvm\")\"" >> "${HOME}/.config/husky/init.sh"
+  echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && . \"\$NVM_DIR/nvm.sh\"" >> "${HOME}/.config/husky/init.sh"
+fi
 
 # Add commit message linting to commit-msg hook
 npm pkg set scripts.commitlint="commitlint --edit"
-echo "yarn commitlint \${1}" > .husky/commit-msg
+echo "npm run commitlint \${1}" > .husky/commit-msg
 
 # test a commit
 COMMITWORKED=1;
-echo "---- testing a commit; please ignore any errors ----";
-git commit -m "test commit" --dry-run || COMMITWORKED=0;
+echo "---- testing a commit ----";
+git commit -m "test commit" --dry-run >/dev/null 2>&1 || COMMITWORKED=0;
 
 if [ ${COMMITWORKED} -eq 1 ]; then
   # we have a problem, that shouldn't have worked.
@@ -158,7 +245,7 @@ if [ ${COMMITWORKED} -eq 1 ]; then
   echo "Our commit linting isn't working as expected \n\n" >&2
   exit 2
 else
-  echo "---- OK. ----"
+  echo "---- commit linting looks good ----"
 fi
 
 #### git-cliff for changelog generation / management
@@ -167,22 +254,25 @@ fi
 num_changelogs=$(find . -iname 'changelog*' -maxdepth 1 | wc -l);
 if [ $num_changelogs -eq 0 ]; then
   CHANGELOG="CHANGELOG.md"
+  echo "---- creating empty ${CHANGELOG} ... ----"
   touch $CHANGELOG
 elif [ $num_changelogs -eq 1 ]; then
   CHANGELOG="$(find . -iname 'changelog*' -maxdepth 1);"
+  echo "---- found ${CHANGELOG} ... ----"
 fi
 
-yarn add -D git-cliff
+echo "---- Setting up git-cliff ----"
+npm add --save-dev git-cliff
 ./node_modules/.bin/git-cliff --init
 npm pkg set "scripts.release-changelog"="git-cliff --unreleased --prepend ${CHANGELOG} --tag "
 
 #### release-it for tagging and github releases
-
-yarn add -D release-it
+echo "---- Setting up release-it ----"
+npm add --save-dev release-it
 npm pkg set "scripts.release"="dotenvx run release-it -- -i -u"
 
 # add the following to ".release-it.json"
-
+echo "---- creating .release-it.json ... ----"
 cat <<- "RELEASEIT" > .release-it.json
   {
     "$schema": "https://unpkg.com/release-it@17/schema/release-it.json",
@@ -197,7 +287,18 @@ cat <<- "RELEASEIT" > .release-it.json
       "publish": true
     },
     "hooks": {
-      "after:bump": "yarn run release-changelog ${version}"
+      "after:bump": "npm run release-changelog ${version}"
     }
   }
 RELEASEIT
+
+echo -e '---- Done! ----\n'
+echo 'You will probably want to check the "test" script in package.json, '
+echo 'by default it will prevent you committing. You can override it with: '
+echo -e "\n\nnpm pkg set scripts.test='echo TODO: WRITE TESTS'\n\n"
+echo 'Then try:'
+echo ' - creating the first commit WITHOUT using conventional commit comments (it should fail!)'
+echo ' - creating the first commit USING conventional commit comments'
+echo ' - pushing your commits'
+echo ' - creating a release:'
+echo '   npm run release'
